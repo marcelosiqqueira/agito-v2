b5e19bbfac86bf543c7f1fcf0e3d1668c1caf1d7
Stablest <marlonlocci@hotmail.com>
2023-05-06 11:40:18 -0300

    added buttonClick props

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,10 +1,18 @@
 type ButtonProps = {
+    buttonClick: ((value: string) => void) | null,
     children: string,
 }
 
-export function Button({ children }: ButtonProps) {
-
+export function Button({ buttonClick, children }: ButtonProps) {
     return (
-        <button className={`bg-orange`}>{children}</button>
+        <button className={`bg-orange`} onClick={buttonClick ?
+            ((e) => buttonClick(e.currentTarget.value)) :
+            (() => console.log(`Button error. Button Click does not exist. Value : ${children}`))}
+            value={children}
+        >{children}</button>
     )
 }
+
+
+
+
