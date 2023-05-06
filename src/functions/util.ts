e0fc142f4afdc324d079ce6c8b55b0c48e471bf3
Stablest <marlonlocci@hotmail.com>
2023-05-06 11:47:25 -0300

    added miniFetch function

---
+++ src/functions/util.ts
@@ -0,0 +1,9 @@
+export async function miniFetch(url: string) {
+  try {
+    const res = await fetch(url);
+    const data = await res.json();
+    return data;
+  } catch (e) {
+    console.log(e);
+  }
+}
