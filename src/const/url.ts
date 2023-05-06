11f66ece8bdf3411c77c46c9d2fe32186edde6c5
Stablest <marlonlocci@hotmail.com>
2023-05-06 11:47:02 -0300

    added Url enum

---
+++ src/const/url.ts
@@ -0,0 +1,6 @@
+export enum Url {
+  BASE = "http://localhost:3333/",
+  EVENTS = "http://localhost:3333/events/",
+  CUSTOM = "http://localhost:3333/custom/",
+  IMAGE = "http://localhost:3333/image/",
+}
