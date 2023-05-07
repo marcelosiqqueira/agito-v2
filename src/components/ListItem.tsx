c9211185d5d76b02a3fa0ae0f71efd5387ffb557
Stablest <marlonlocci@hotmail.com>
2023-05-07 10:26:22 -0300

    added time and type props

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,13 +1,17 @@
+import { EventsEnum } from "../const/Enums/eventsEnum"
+
 type ListItemProps = {
     id: string,
     name: string,
     date: Date,
+    time: string,
     local: string,
     clicks: number | null,
+    type: EventsEnum
     handleSelectedEvent: (value: string) => void,
 }
 
-export function ListItem({ id, name, date, local, clicks, handleSelectedEvent }: ListItemProps) {
+export function ListItem({ id, name, date, time, local, clicks, type, handleSelectedEvent }: ListItemProps) {
     return (
         <li>
             <button onClick={() => handleSelectedEvent(id)}>
@@ -18,17 +22,18 @@ export function ListItem({ id, name, date, local, clicks, handleSelectedEvent }:
                 <div>
                     <div>
                         <img src="" alt="" />
-                        <span>{date.toLocaleString('pt-BR')}</span>
+                        <span>{date.toLocaleDateString('pt-BR')}</span>
+                        {type === EventsEnum.SCHEDULE && <span>Horario : {time}</span>}
                     </div>
                     <div>
                         <img src="" alt="" />
                         <span>{local}</span>
                     </div>
                 </div>
-                <div>
+                {type === EventsEnum.COVERAGES && <div>
                     <img src="" alt="" />
-                    <span>{clicks ?? 'N/A'}</span>
-                </div>
+                    <span>{clicks ?? 'Clicks : N/A'}</span>
+                </div>}
             </button>
         </li>
     )
