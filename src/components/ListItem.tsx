b51083b6f82227c940672689cf456fbd5a5971f8
Bianca Pena <120587014+Krepixie@users.noreply.github.com>
2023-05-08 00:36:30 -0300

    add css to events list and img carousel

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -13,25 +13,25 @@ type ListItemProps = {
 
 export function ListItem({ id, name, date, time, local, clicks, type, handleSelectedEvent }: ListItemProps) {
     return (
-        <li>
-            <button onClick={() => handleSelectedEvent(id)}>
-                <div>
-                    <img src="" alt="" />
+        <li className="relative px-5">
+            <button onClick={() => handleSelectedEvent(id)} className="bg-white border-dark-gray border-4 rounded-lg w-full h-24 p-4 text-xl text-light-purple">
+                <div className="flex font-bold gap-1.5">
+                    <img src="src/assets/star.svg" alt="" />
                     <span>{name}</span>
                 </div>
-                <div>
-                    <div>
-                        <img src="" alt="" />
+                <div className="flex gap-2.5 font-bold">
+                    <div className="flex gap-1.5">
+                        <img src="src/assets/clock.svg" alt="" />
                         <span>{date.toLocaleDateString('pt-BR')}</span>
                         {type === EventsEnum.SCHEDULE && <span>Horario : {time}</span>}
                     </div>
-                    <div>
-                        <img src="" alt="" />
+                    <div className="flex gap-1.5">
+                        <img src="src/assets/location.svg" alt="" />
                         <span>{local}</span>
                     </div>
                 </div>
-                {type === EventsEnum.COVERAGES && <div>
-                    <img src="" alt="" />
+                {type === EventsEnum.COVERAGES && <div className="absolute top-1 right-8 flex gap-2">
+                    <img src="src/assets/clicks.svg" alt=""/>
                     <span>{clicks ?? 'Clicks : N/A'}</span>
                 </div>}
             </button>
