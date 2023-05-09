5faee80a5b06922892f4f05665dd4eb3bb0897a8
Marcelo Siqueira Filho <marcelosiqqueira@gmail.com>
2023-05-09 02:16:24 -0300

    change urlEnum to corrects api routes

--- src/const/Enums/urlEnum.ts
+++ src/const/Enums/urlEnum.ts
@@ -1,5 +1,5 @@
 export enum UrlEnum {
   BASE = "https://gabrielagito.com.br/server-agito/",
   EVENTS = "https://gabrielagito.com.br/server-agito/events/",
-  IMAGE = "http://181.215.135.241:8080/image/",
+  IMAGE = "https://gabrielagito.com.br/server-agito/image/",
 }
