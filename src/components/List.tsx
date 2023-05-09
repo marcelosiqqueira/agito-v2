cfd72cb075785b0c57e3c425844f00a1b72ac880
Stablest <marlonlocci@hotmail.com>
2023-05-09 05:55:55 -0300

    Added handlePage props and passed as props to ListButton

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,12 +1,14 @@
 import { ListButton } from "./ListButton"
 import { EventsEnum } from "../const/Enums/eventsEnum"
+import { CarouselButtonAction } from "../const/Enums/carouselButtonAction"
 
 type ListProps = {
     children: React.ReactNode
+    handlePage: (value: string) => void,
     handleCoverageSelected: (value: string) => void
 }
 
-export function List({ handleCoverageSelected, children }: ListProps) {
+export function List({ handlePage, handleCoverageSelected, children }: ListProps) {
     return (
         <div className="bg-gray">
             <div className="text-xl pb-5">
@@ -20,13 +22,13 @@ export function List({ handleCoverageSelected, children }: ListProps) {
             </div>
             <div>
                 <ul className="flex gap-1.5 justify-around">
-                    <ListButton>{'|<'}</ListButton>
-                    <ListButton>{'<'}</ListButton>
-                    <ListButton>1</ListButton>
-                    <ListButton>2</ListButton>
-                    <ListButton>3</ListButton>
-                    <ListButton>{'>'}</ListButton>
-                    <ListButton>{'>|'}</ListButton>
+                    <ListButton value={CarouselButtonAction.START} buttonClick={handlePage}>{'|<'}</ListButton>
+                    <ListButton value={CarouselButtonAction.PREV} buttonClick={handlePage}>{'<'}</ListButton>
+                    <ListButton value={CarouselButtonAction.SELECT} buttonClick={handlePage}>1</ListButton>
+                    <ListButton value={CarouselButtonAction.SELECT} buttonClick={handlePage}>2</ListButton>
+                    <ListButton value={CarouselButtonAction.SELECT} buttonClick={handlePage}>3</ListButton>
+                    <ListButton value={CarouselButtonAction.NEXT} buttonClick={handlePage}>{'>'}</ListButton>
+                    <ListButton value={CarouselButtonAction.END} buttonClick={handlePage}>{'>|'}</ListButton>
                 </ul>
             </div>
         </div>
