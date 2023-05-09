66e2bcf724c03c6449ffd6840275b66610bebbc9
Stablest <marlonlocci@hotmail.com>
2023-05-09 05:55:01 -0300

    Added second argument to buttonClick based on value

--- src/components/ListButton.tsx
+++ src/components/ListButton.tsx
@@ -1,11 +1,18 @@
+import { CarouselButtonAction } from "../const/Enums/carouselButtonAction"
+
 type ListButtonProps = {
-    children: string
+    children: string,
+    value: string,
+    buttonClick: (value: string, index?: number) => void
 }
 
-export function ListButton({ children }: ListButtonProps) {
+export function ListButton({ value, children, buttonClick }: ListButtonProps) {
     return (
         <li className="bg-dark-gray w-10 h-10 font-bold text-sm flex justify-center align-center rounded border-4 border-dark-gray">
-            <button className="bg-white w-full h-full rounded hover:bg-light-purple duration-300">
+            <button className="bg-white w-full h-full rounded hover:bg-light-purple duration-300"
+                value={value}
+                onClick={(e) => buttonClick(e.currentTarget.value,
+                    (e.currentTarget.value === CarouselButtonAction.SELECT ? Number.parseInt(children) : undefined))}>
                 {children}
             </button>
         </li>
