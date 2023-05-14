0de54a876fc26dd3a5a4253aef3725d4432cbea4
Marcelo Siqueira Filho <marcelosiqqueira@gmail.com>
2023-05-14 15:40:04 -0300

    create public folder for assets

--- src/const/Enums/urlEnum.ts
+++ src/const/Enums/urlEnum.ts
@@ -3,4 +3,5 @@ export enum UrlEnum {
   EVENTS = "https://gabrielagito.com.br/server-agito/events/",
   IMAGE = "https://gabrielagito.com.br/server-agito/image/",
   CLICKS = "https://gabrielagito.com.br/server-agito/clicks/",
+  CLICKS_UPDATE = "https://gabrielagito.com.br/server-agito/clicks-update"
 }
