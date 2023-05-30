43a8f509b412b68055894254b3a709be2ac7aa40
Bianca Pena <biancalessapena@gmail.com>
2023-05-30 00:51:31 -0300

    added new custom themes in tailwind config file

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,7 @@ export default {
       white: "#FFFFFF",
       black: "#000000",
       orange: "#FF7600",
-      yellow: "FFA900",
+      yellow: "#FFA900",
       gray: "#555555",
       purple: "#52006A",
       pink: "#CD113B",
@@ -23,10 +23,15 @@ export default {
         'last-page': "url('/last-page.svg')",
         'arrow': "url('/arrow.svg')",
         'close': "url('/close.svg')",
+        'dots-design': "url('/dots-design.svg')"
       },
       dropShadow: {
         'under': '4px 5px 0px rgba(205, 17, 59, 1)',
         'default': '0 4px 4px rgba(0, 0, 0, 0.25)',
+      },
+      backgroundPosition: {
+        'righttop': 'right top 1rem',
+        'lefttop': 'left top 1rem',
       },
     },
   },
