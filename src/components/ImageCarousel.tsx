4d8b78eb6c34cb2b45976060eb6ad13756c5e454
Bianca Pena <biancalessapena@gmail.com>
2023-05-29 22:49:35 -0300

    added event info on hover to the image carousel

--- src/components/ImageCarousel.tsx
+++ src/components/ImageCarousel.tsx
@@ -80,34 +80,34 @@ export function ImageCarousel({ imagesUrl, autoPlay, multiple }: ImageCarouselPr
     }, [imagesUrl])
 
     return (
-        <div className="text-white bg-black/50 flex-col justify-center items-center relative lg:w-[720px] lg:h-[480px] lg:rounded-lg lg:overflow-hidden" ref={divRef} tabIndex={1}>
-            <button className="object-contain aspect-[3-2]">
-                <img src={imagesUrl ? imagesUrl[index] ?? '/error' : '/error'} alt="Foto do evento" onClick={handleOpenModal} onLoad={imageTimerHandler} className="lg:max-w-full lg:max-h-full aspect-[3-2]" />
-            </button>
-            <div className="flex justify-evenly relative -top-[86px] lg:-bottom-28 bg-black/40">
-                {multiple && (imagesUrl && imagesUrl.slice(subIndex, maxMultipleSize() + subIndex).map((_, elementIndex) =>
-                    <button
-                        key={elementIndex}
-                        value={CarouselButtonAction.SELECT}
-                        onClick={(e) => handleIndex(e.currentTarget.value, elementIndex + subIndex)} className="bg-black/40">
-                        <img src={imagesUrl[elementIndex + subIndex]}
-                            alt="" height={55} width={83} className="h-20 w-22 object-contain aspect-[3-2] max-w-full" />
-                    </button>))
-                }
-                {multiple &&
-                    <>
-                        <button value={CarouselButtonAction.PREV} onClick={(e) => handleSubIndex(e.currentTarget.value)} className="h-10 w-10 absolute cursor-pointer top-1/3 left-2 bg-arrow-left bg-dark-gray/60 bg-center bg-contain rounded-full hover:bg-white/70 duration-200">
-                            {''}
-                        </button>
-                        <button value={CarouselButtonAction.NEXT} onClick={(e) => handleSubIndex(e.currentTarget.value)} className="h-10 w-10 absolute cursor-pointer top-1/3 right-2 bg-dark-gray/60 bg-arrow-right bg-center bg-contain rounded-full hover:bg-white/70 duration-200">
-                            {''}
-                        </button>
-                    </>
-                }
+            <div className="text-white bg-black/90 flex-col justify-center items-center relative lg:w-[720px] lg:h-[480px] w-full lg:rounded-lg lg:overflow-hidden object-contain" ref={divRef} tabIndex={1}>
+                <button className="flex items-center justify-center w-full h-full">
+                    <img src={imagesUrl ? imagesUrl[index] ?? '/error' : '/error'} alt="Foto do evento" onClick={handleOpenModal} onLoad={imageTimerHandler} className=" aspect-[3-2] object-contain h-[215px] lg:h-[480px] w-full" />
+                </button>
+                <div className="flex justify-evenly relative  lg:-bottom-28 bg-black/40">
+                    {multiple && (imagesUrl && imagesUrl.slice(subIndex, maxMultipleSize() + subIndex).map((_, elementIndex) =>
+                        <button
+                            key={elementIndex}
+                            value={CarouselButtonAction.SELECT}
+                            onClick={(e) => handleIndex(e.currentTarget.value, elementIndex + subIndex)} className="bg-black/40">
+                            <img src={imagesUrl[elementIndex + subIndex]}
+                                alt="" height={55} width={83} className="h-20 w-22 object-contain aspect-[3-2] max-w-full" />
+                        </button>))
+                    }
+                    {multiple &&
+                        <>
+                            <button value={CarouselButtonAction.PREV} onClick={(e) => handleSubIndex(e.currentTarget.value)} className="h-10 w-10 absolute cursor-pointer top-1/3 left-2 bg-arrow bg-dark-gray/60 bg-center bg-contain rounded-full hover:bg-white/70 duration-200">
+                                {''}
+                            </button>
+                            <button value={CarouselButtonAction.NEXT} onClick={(e) => handleSubIndex(e.currentTarget.value)} className="h-10 w-10 absolute cursor-pointer top-1/3 right-2 bg-dark-gray/60 bg-arrow-right bg-center bg-contain rounded-full hover:bg-white/70 duration-200">
+                                {''}
+                            </button>
+                        </>
+                    }
+                </div>
+                <button value={CarouselButtonAction.PREV} onClick={(e) => handleIndex(e.currentTarget.value)} className="h-20 w-9 absolute cursor-pointer left-0 top-[30%] lg:top-[40%] bg-arrow scale-x-[-1] bg-black/60 bg-center bg-no-repeat rounded-l-xl hover:bg-dark-gray/60 duration-200">{''}</button>
+                <button value={CarouselButtonAction.NEXT} onClick={(e) => handleIndex(e.currentTarget.value)} className="h-20 w-9 absolute cursor-pointer right-0 top-[30%] lg:top-[40%] bg-black/60 bg-arrow bg-no-repeat bg-center rounded-l-xl hover:bg-dark-gray/60 duration-200">{''}</button>
+                {isOpen && <Modal handleIndex={handleIndex} handleOpenModal={handleOpenModal} index={index} imagesUrl={imagesUrl}></Modal>}
             </div>
-            <button value={CarouselButtonAction.PREV} onClick={(e) => handleIndex(e.currentTarget.value)} className="h-20 w-9 absolute cursor-pointer left-0 top-[30%] bg-arrow-left bg-black/60 bg-center rounded-r-xl hover:bg-dark-gray/60 duration-200">{''}</button>
-            <button value={CarouselButtonAction.NEXT} onClick={(e) => handleIndex(e.currentTarget.value)} className="h-20 w-9 absolute cursor-pointer right-0 top-[30%] bg-black/60 bg-arrow-right bg-center rounded-l-xl hover:bg-dark-gray/60 duration-200">{''}</button>
-            {isOpen && <Modal handleIndex={handleIndex} handleOpenModal={handleOpenModal} index={index} imagesUrl={imagesUrl}></Modal>}
-        </div>
     )
 }
