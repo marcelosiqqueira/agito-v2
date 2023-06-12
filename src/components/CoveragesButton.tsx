3380574330c5f78b842c1565eeef274341dfe648
Stablest <marlonlocci@hotmail.com>
2023-06-12 08:20:15 -0300

    feature: Added CoveragesButton component

---
+++ src/components/CoveragesButton.tsx
@@ -0,0 +1,20 @@
+import { CarouselButtonAction } from "../const/Enums/carouselButtonAction"
+
+type ListButtonProps = {
+    children: string,
+    value: string,
+    buttonClick: (value: string, index?: number) => void
+}
+
+export function CoveragesButton({ value, children, buttonClick }: ListButtonProps) {
+    return (
+        <li className=" w-10 h-10 font-bold text-sm flex justify-center items-center">
+            <button className="bg-white w-full h-full hover:bg-light-purple duration-300"
+                value={value}
+                onClick={(e) => buttonClick(e.currentTarget.value,
+                    (e.currentTarget.value === CarouselButtonAction.SELECT ? Number.parseInt(children) : undefined))}>
+                {children}
+            </button>
+        </li>
+    )
+}
