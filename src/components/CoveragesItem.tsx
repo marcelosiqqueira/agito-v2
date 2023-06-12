572064867f596751c0855caed99741c25f42f07a
Stablest <marlonlocci@hotmail.com>
2023-06-12 08:20:37 -0300

    feature: Added CoveragesItem component

---
+++ src/components/CoveragesItem.tsx
@@ -0,0 +1,30 @@
+type ListItemProps = {
+    id: string,
+    name: string,
+    date: Date,
+    local: string,
+    clicks: number | null,
+}
+
+export function CoveragesItem({ id, name, date, local, clicks }: ListItemProps) {
+    return (
+        <li>
+            <button onClick={() => console.log('oi')} className="bg-white text-black border-dark-gray border w-full h-28 px-6 py-2 text-xl text-start hover:bg-purple/40 hover:text-white duration-200">
+                <div className="leading-none">
+                    <div className="capitalize font-semibold">{name.toLowerCase()}</div>
+                    <div className="font-light text-gray opacity-80">{local}</div>
+                </div>
+                <div className="flex justify-between">
+                    <div className="flex">
+                        <img src="./calendar.svg" alt="calendar icon" />
+                        {date.toLocaleString('pt-BR', { dateStyle: "short" })}
+                    </div>
+                    <div className="flex">
+                        <img src="./views.svg" alt="eye icon" />
+                        {clicks}
+                    </div>
+                </div>
+            </button>
+        </li>
+    )
+}
