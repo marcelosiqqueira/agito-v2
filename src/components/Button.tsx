898d956f4c77e160745bec8d8f893b4c1158c9a4
Stablest <marlonlocci@hotmail.com>
2023-06-14 12:25:29 -0300

    feature: Button component now accepts button attributes

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,17 @@
+import { ButtonHTMLAttributes } from "react"
+
 type ButtonProps = {
     value: string,
     children: string,
+    buttonAtt?: CustomButtonAtt,
     buttonClick: ((value: string) => void) | null,
 }
 
-export function Button({ buttonClick, children, value }: ButtonProps) {
+type CustomButtonAtt = Omit<ButtonHTMLAttributes<HTMLButtonElement>, 'onClick' & 'value'>
+
+export function Button({ buttonClick, children, value, buttonAtt }: ButtonProps & CustomButtonAtt) {
     return (
-        <button className={`bg-transparent h-full px-7 uppercase font-bold text-xl active:scale-90 duration-300`} onClick={buttonClick ?
+        <button {...buttonAtt} className={buttonAtt?.className ?? `bg-transparent lg:h-full px-7 uppercase font-bold text-xl active:scale-90 duration-300`} onClick={buttonClick ?
             ((e) => buttonClick(e.currentTarget.value)) :
             (() => console.log(`Button error. Button onClick does not exist. Value : ${children}`))}
             value={value}
