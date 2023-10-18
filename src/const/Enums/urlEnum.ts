e5afd72f709c4d426644a15318bc046f96f80de2
Stablest <marlonlocci@hotmail.com>
2023-10-18 13:50:15 -0300

    bug-fix: Changed urlEnum CLICKS and CLICK_UPDATE to correct host

--- src/const/Enums/urlEnum.ts
+++ src/const/Enums/urlEnum.ts
@@ -8,9 +8,9 @@ export enum UrlEnum {
   // IMAGE = "https://gabrielagito.com.br/server-agito/image/",
   IMAGE = "https://drive.google.com/uc?export=view&id=",
 
-  // CLICKS = "https://gabrielagito.com.br/server-agito/clicks/",
-  CLICKS = "http://localhost:8080/clicks",
+  CLICKS = "https://gabrielagito.com.br/server-agito/clicks/",
+  // CLICKS = "http://localhost:8080/clicks",
 
-  // CLICKS_UPDATE = "https://gabrielagito.com.br/server-agito/clicks-update"
-  CLICKS_UPDATE = "https://localhost:8080/clicks-updates",
+  CLICKS_UPDATE = "https://gabrielagito.com.br/server-agito/clicks-update",
+  // CLICKS_UPDATE = "https://localhost:8080/clicks-updates",
 }
